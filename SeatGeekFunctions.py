2818f46312d659195877364f843ef017306f81b9
Rick Boulanger <Boulara@me.com>
2014-12-15 21:51:07 -0500

    Updates

--- SeatGeekFunctions.py
+++ SeatGeekFunctions.py
@@ -40,7 +40,7 @@ def fncCheckValues(id,old,new,parm, title):
 	trendName = parm + '_trend'
 	if (new > old):
 		db.concerts.update({'concert_id': id}, {'$set': {parm: new, trendName: fncTrend(old,new)}})
-		text = titele + " " + parm + " Changed"
+		text = title + " " + parm + " Changed"
 		fncSendPushover(text)
 		print('DB Updated >> ', parm)
 	else:
